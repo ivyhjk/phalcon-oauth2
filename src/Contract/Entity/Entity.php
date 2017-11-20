3af4f143114fe45a7ee7cf1175e4b4e80b7deb14
Elvis Munoz <elvis.munoz.f@gmail.com>
2017-11-20 14:38:38 -0300

    Add main interface for entities

---
+++ src/Contract/Entity/Entity.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Ivyhjk\Phalcon\OAuth2\Server\Contract\Entity;
+
+/**
+ * An access token implementation.
+ *
+ * @since v1.0.0
+ * @version v1.0.0
+ * @package Ivyhjk\Phalcon\OAuth2\Server\Contract
+ * @author Elvis Munoz <elvis.munoz.f@gmail.com>
+ * @copyright Copyright (c) 2017, Elvis Munoz
+ * @license https://opensource.org/licenses/MIT MIT License
+ */
+interface Entity
+{
+    /**
+     * Get the associated numeric identifier.
+     *
+     * @return int
+     */
+    public function getId() : int;
+}
