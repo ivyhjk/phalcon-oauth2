7f3c4703fe389691f55146843f4ea24ac84fd8c3
Elvis Munoz <elvis.munoz.f@gmail.com>
2017-11-20 15:03:32 -0300

    Add custom interface for Scope

---
+++ src/Contract/Entity/Scope.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Ivyhjk\Phalcon\OAuth2\Server\Contract\Entity;
+
+/**
+ * A scope entity representation.
+ *
+ * @since v1.0.0
+ * @version v1.0.0
+ * @package Ivyhjk\Phalcon\OAuth2\Server\Contract\Entity
+ * @author Elvis Munoz <elvis.munoz.f@gmail.com>
+ * @copyright Copyright (c) 2017, Elvis Munoz
+ * @license https://opensource.org/licenses/MIT MIT License
+ */
+interface Scope extends
+    \League\OAuth2\Server\Entities\ScopeEntityInterface,
+    Entity
+{
+
+}
